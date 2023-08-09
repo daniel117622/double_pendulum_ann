b34318c6d01b40b33b22455007a4e57e89641978
daniel117622 <dan5jul@hotmail.com>
2023-08-09 16:20:38 -0600

    nn_model

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import gymnasium
 import time
-env = gymnasium.make("InvertedDoublePendulum-v4", render_mode='human')
-observation = env.reset()
-done = False
 
-while True:
-    action = env.action_space.sample()  
-    observation, reward, done, info, _= env.step(action)
+env         = gymnasium.make("InvertedDoublePendulum-v4", render_mode='human')
+observation = env.reset()
+done        = False
+trials      = 0
+
+print(env.action_space.sample())
+action = [0]
+while trials <= 1000:
+    observation, reward, done, info, _ = env.step(action)
+    action = [-observation[5]]
     env.render()
-    time.sleep(0.5)
+    time.sleep(0.05)
     if done:
+        trials += 1
         env.reset()
     print(f"Pole-Cart angle :{observation[1]*90:.3f}")
     
-
 env.close()
---
+++ nn_model.py
@@ -0,0 +1,3 @@
+class nn_model():
+    def __init__(self) -> None:
+        pass
